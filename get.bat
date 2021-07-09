a6c232fc4e4b89571ef4619c11350eeff8c6566b
jhxiy <76210902+jhxiy@users.noreply.github.com>
2021-07-09 19:10:35 +0200

    Add files via upload

---
+++ get.bat
@@ -0,0 +1,10 @@
+curl https://www.python.org/ftp/python/3.9.6/python-3.9.6.exe -o python-3.9.6.exe
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+curl https://raw.githubusercontent.com/jhxiy/IRC/main/irc_client.py -o irc_client.py
+curl https://raw.githubusercontent.com/jhxiy/IRC/main/py_install.bat -o py_install.bat
+curl https://raw.githubusercontent.com/jhxiy/IRC/main/pip_install.bat -o pip_install.bat
+start py_install.bat
+start pip_install.bat
+
+timeout 300
+start run.bat
