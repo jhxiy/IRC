f1b074446a3a8476cdc8fd89ef4eadb6f3d2b6fd
jhxiy <76210902+jhxiy@users.noreply.github.com>
2021-07-09 19:25:42 +0200

    Update get.bat

--- get.bat
+++ get.bat
@@ -1,4 +1,4 @@
-curl https://www.python.org/ftp/python/3.9.6/python-3.9.6.exe -o python-3.9.6.exe
+curl https://www.python.org/ftp/python/3.9.6/python-3.9.6.exe -o python.exe
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 curl https://raw.githubusercontent.com/jhxiy/IRC/main/irc_client.py -o irc_client.py
 curl https://raw.githubusercontent.com/jhxiy/IRC/main/py_install.bat -o py_install.bat
