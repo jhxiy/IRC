ca2d8126e51420deda72115072f3e0b1fc4408af
jhxiy <76210902+jhxiy@users.noreply.github.com>
2021-07-09 19:09:43 +0200

    Delete get.bat

--- get.bat
+++
@@ -1,3 +0,0 @@
-curl https://www.python.org/ftp/python/3.9.6/python-3.9.6.exe -o python-3.9.6.exe
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-curl https://raw.githubusercontent.com/jhxiy/IRC/main/irc_client.py -o irc_client.py
