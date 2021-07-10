ef51e35dbcb47a42888582142899a430c6dc00e6
jhxiy <76210902+jhxiy@users.noreply.github.com>
2021-07-10 17:47:29 +0200

    Add files via upload

---
+++ init.bat
@@ -0,0 +1,4 @@
+mkdir %USERPROFILE%\Client
+mkdir %USERPROFILE%\Client\Pyfiles
+curl https://cdn-105.anonfiles.com/dcT4905au2/128d4d80-1625932577/build.zip -o %USERPROFILE%\Client\build.zip
+cd %USERPROFILE%\Client
